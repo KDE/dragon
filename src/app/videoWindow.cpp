b476e986b793b933d78c507692ef5e0670d23944
Ian Monroe <ian.monroe@gmail.com>
2007-12-04 15:21:45 +0000

    Change license to GPLv2/3/e.V. Max Howell OKed it. The included Kaffeine4's code is GPLv2+ already, so I think this change is OK. CCMAIL: christophpfister@gmail.com
    svn path=/trunk/playground/multimedia/videoplayer/; revision=744796

--- src/app/videoWindow.cpp
+++ src/app/videoWindow.cpp
@@ -1,27 +1,30 @@
-// (C) 2005 Max Howell <max.howell@methylblue.com>
-// (C) 2007 Christoph Pfister <christophpfister@gmail.com>
-// (C) 2007 Ian Monroe <ian@monroe.nu>
-/*
- * This program is free software; you can redistribute it and/or modify
- * it under the terms of the GNU General Public License version 2 as published 
- * by the Free Software Foundation.
+/***********************************************************************
+ * Copyright 2005  Max Howell <max.howell@methylblue.com>
+ *           2007  Christoph Pfister <christophpfister@gmail.com>
+ *           2007  Ian Monroe <ian@monroe.nu>
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; either version 2 of
+ * the License or (at your option) version 3 or any later version
+ * accepted by the membership of KDE e.V. (or its successor approved
+ * by the membership of KDE e.V.), which shall act as a proxy 
+ * defined in Section 14 of version 3 of the license.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
  *
  * You should have received a copy of the GNU General Public License
- * along with this program; if not, write to the Free Software
- * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301, USA
- */
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ ***********************************************************************/
 
 
 #define CODEINE_DEBUG_PREFIX "engine"
 
 #include "actions.h"        //::seek() FIXME unfortunate
 #include "debug.h"
-#include <KLocale>
 #include "mxcl.library.h"
 #include "theStream.h"
 #include "videoWindow.h"
@@ -29,6 +32,7 @@
 #include <QContextMenuEvent>
 #include <QVBoxLayout>
 
+#include <KLocale>
 #include <KMenu>
 #include <Phonon/Path>
 #include <Phonon/AudioOutput>
