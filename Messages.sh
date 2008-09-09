27c0eae88b0d55ac084ea7b40bf53c1c9b6bede5
Laurent Montel <montel@kde.org>
2008-09-09 07:21:08 +0000

    There is not .cc file
    svn path=/trunk/KDE/kdemultimedia/dragonplayer/; revision=858951

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,3 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
-$XGETTEXT `find . -name \*.cc -o -name \*.cpp -o -name \*.h` -o $podir/dragonplayer.pot
+$XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/dragonplayer.pot
