a3477d5347ca1aa5a74573ef153322521f5ff1de
Laurent Montel <montel@kde.org>
2009-04-18 12:30:23 +0000

    Minor
    svn path=/trunk/KDE/kdemultimedia/dragonplayer/; revision=955661

--- Messages.sh
+++ Messages.sh
@@ -1,3 +1,4 @@
 #! /usr/bin/env bash
 $EXTRACTRC `find . -name \*.rc -o -name \*.ui -o -name \*.kcfg` >> rc.cpp
 $XGETTEXT `find . -name \*.cpp -o -name \*.h` -o $podir/dragonplayer.pot
+rm -f rc.cpp
