8947e3e67c03cb65e7355fa94a27f10da3dc73c0
Harald Sitter <sitter@kde.org>
2012-05-31 22:18:17 +0200

    increase startup speed by not messing with powermanagement

--- src/app/stateChange.cpp
+++ src/app/stateChange.cpp
@@ -127,10 +127,12 @@ MainWindow::engineStateChanged( Phonon::State state, Phonon::State oldstate )
     else if( Phonon::StoppedState || !TheStream::hasMedia() )
     {
       //stop supressing sleep
-      Solid::PowerManagement::stopSuppressingSleep(m_stopSleepCookie);
+      if (m_stopSleepCookie != -1)
+        Solid::PowerManagement::stopSuppressingSleep(m_stopSleepCookie);
 
       //stop supressing screen power management
-      Solid::PowerManagement::stopSuppressingScreenPowerManagement(m_stopScreenPowerMgmtCookie);
+      if (m_stopScreenPowerMgmtCookie != -1)
+        Solid::PowerManagement::stopSuppressingScreenPowerManagement(m_stopScreenPowerMgmtCookie);
 
      //stop disabling screensaver
       delete m_stopScreenSaver; // It is always 0, I have been careful.
