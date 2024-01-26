8167c6ae6a8ea09d2027baa25c15bc9060c1b2ad
Laurent Montel <montel@kde.org>
2024-01-26 08:35:42 +0100

    Remove qt5 code

--- src/app/main.cpp
+++ src/app/main.cpp
@@ -26,9 +26,6 @@
 
 int main(int argc, char **argv)
 {
-#if QT_VERSION < QT_VERSION_CHECK(6, 0, 0)
-    QCoreApplication::setAttribute(Qt::AA_UseHighDpiPixmaps);
-#endif
     Dragon::PlayerApplication app(argc, argv);
     KCrash::initialize();
 
